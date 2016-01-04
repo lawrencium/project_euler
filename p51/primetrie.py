1e8bb0d1171ee50730d764f349343861155ed827
lawrencium <ltc35@cornell.edu>
2016-01-03 19:38:53 -0800

    implemented leaf insertion

--- p51/primetrie.py
+++ p51/primetrie.py
@@ -1,14 +1,76 @@
 __author__ = 'lawrencechen'
 
 
+class InsertInformation(object):
+    def __init__(self, number, asterisk_value):
+        self._asterisk_value = asterisk_value
+        self._number = number
+
+        number_string = str(number)
+        self._string_pattern = number_string.replace(asterisk_value, '*') if asterisk_value else number_string
+
+        self.__current_level = 0
+
+    @property
+    def next_digit(self):
+        digit = self._string_pattern[self.__current_level]
+        self.__current_level += 1
+        return digit
+
+    @property
+    def asterisk_value(self):
+        return self._asterisk_value
+
+    @property
+    def number(self):
+        return self._number
+
+
 class PrimeNode(object):
     def __init__(self):
+        self.digits = {}
+
+    def insert(self, number):
         pass
 
 
 class PrimeLeafNode(PrimeNode):
-    def __init__(self):
-        super(PrimeLeafNode, self).__init__()
+    class Information(object):
+        def __init__(self, smallest_value):
+            self._smallest_value = smallest_value
+            self._count = 1
+
+        @property
+        def smallest_value(self):
+            return self._smallest_value
+
+        @property
+        def count(self):
+            return self._count
+
+        def increment(self):
+            self._count += 1
+
+    def insert(self, insert_information):
+        digit = insert_information.next_digit
+        number = insert_information.number
+        information = self.__increment_value(digit, number)
+
+        if digit == '*':
+            alternative = self.__increment_value(insert_information.asterisk_value, number)
+            if alternative.count > information.count:
+                information = alternative
+
+        return information
+
+    def __increment_value(self, digit, number):
+        if digit in self.digits:
+            information = self.digits[digit]
+            information.increment()
+        else:
+            self.digits[digit] = PrimeLeafNode.Information(number)
+
+        return self.digits[digit]
 
 
 class PrimeIndexNode(PrimeNode):
@@ -18,4 +80,13 @@ class PrimeIndexNode(PrimeNode):
 
 class PrimeDigitTrie(object):
     def __init__(self, levels):
+        self._levels = levels
         self._root = PrimeLeafNode() if levels == 1 else PrimeIndexNode()
+
+    def insert(self, value):
+        if value <= 0:
+            raise Exception('Cannot insert non-zero value')
+
+        number_digits = len(str(value))
+        if number_digits != self._levels:
+            raise Exception('Trying to insert number with %i digits into %i level tree' % (number_digits, self._levels))
--- p51/test_primetree.py
+++ p51/test_primetree.py
@@ -2,7 +2,7 @@ import unittest
 
 from assertpy import assert_that
 
-from p51.primetrie import PrimeDigitTrie, PrimeLeafNode, PrimeIndexNode
+from p51.primetrie import PrimeDigitTrie, PrimeLeafNode, PrimeIndexNode, InsertInformation
 
 __author__ = 'lawrencechen'
 
@@ -13,3 +13,60 @@ class PrimeDigitTrieInitializationTest(unittest.TestCase):
 
     def test_prime_tree_has_index_node_root_if_levels_is_greater_than_one(self):
         assert_that(PrimeDigitTrie(2)._root).is_type_of(PrimeIndexNode)
+
+
+class PrimeDigitTrieInsertTest(unittest.TestCase):
+    def setUp(self):
+        self.tree = PrimeDigitTrie(3)
+
+    def test_tree_throws_exception_on_insert_if_inserting_negative_number(self):
+        with self.assertRaises(Exception):
+            self.tree.insert(-10)
+
+    def test_tree_throws_exception_on_insert_if_digits_in_number_to_insert_does_not_equal_levels(self):
+        with self.assertRaises(Exception):
+            self.tree.insert(10)
+
+
+# noinspection PyStatementEffect
+class PrimeLeafNodeInsertTest(unittest.TestCase):
+    def setUp(self):
+        self.insert_information = InsertInformation(9, '')
+        self.asterisk_insert = InsertInformation(9, '9')
+        self.node = PrimeLeafNode()
+
+    def test_node_inserts_new_digit_to_digits_with_value_one_if_digit_not_in_digits_table(self):
+        information = self.node.insert(self.insert_information)
+        assert_that(information.count).is_equal_to(1)
+
+    def test_return_correct_smallest_value_on_single_insert(self):
+        information = self.node.insert(self.insert_information)
+        assert_that(information).has_smallest_value(9)
+
+    def test_node_increments_value_by_one_if_digit_already_in_table(self):
+        self.node.insert(self.insert_information)
+        second_insert = InsertInformation(19, '')
+        second_insert.next_digit  # skip the first digit to insert at leaf level
+        information = self.node.insert(second_insert)
+        assert_that(information.count).is_equal_to(2)
+
+    def test_return_correct_smallest_value_on_multiple_inserts_to_same_pattern(self):
+        self.node.insert(self.insert_information)
+        second_insert = InsertInformation(19, '')
+        second_insert.next_digit  # skip the first digit to insert at leaf level
+        information = self.node.insert(second_insert)
+        assert_that(information.smallest_value).is_equal_to(9)
+
+    def test_node_returns_asterisk_value_count_if_it_is_larger(self):
+        first_insert = InsertInformation(19, '9')
+        first_insert.next_digit  # skip the first digit to insert at leaf level
+        self.node.insert(first_insert)
+        information = self.node.insert(self.insert_information)
+        assert_that(information.count).is_equal_to(2)
+
+    def test_node_returns_value_associated_with_larger_count_on_insert_asterisk(self):
+        first_insert = InsertInformation(19, '9')
+        first_insert.next_digit  # skip the first digit to insert at leaf level
+        self.node.insert(first_insert)
+        information = self.node.insert(self.insert_information)
+        assert_that(information.smallest_value).is_equal_to(19)
