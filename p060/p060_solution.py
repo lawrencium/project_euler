ec5cc0df9b7082dd731cbd8102f4d9444b94e879
lawrencium <ltc35@cornell.edu>
2016-06-02 18:04:21 +0200

    trying out an n^2 implementation that is too slow!

--- p060/p060_solution.py
+++ p060/p060_solution.py
@@ -1,71 +1,52 @@
-from util.prime import sieve_of_eratosthenes
+from util.prime import sieve_of_eratosthenes, is_prime
 from util.solutiontimer import time_function
 
 __author__ = 'lawrencechen'
 
-HIGHEST_PRIME = 9999999
+PRIME_UPPER_BOUND = 9999999
+FAMILY_SIZE = 4
 
 
 def concatenate(n1, n2):
     return int('{}{}'.format(n1, n2))
 
 
-def concatenation_in_set(n1, n2, s):
-    return concatenate(n1, n2) in s
+def concatenation_is_prime(n1, n2, s):
+    concatenation = concatenate(n1, n2)
+    return concatenation in s or is_prime(concatenation)
 
 
 def both_permutations_in_set(n1, n2, s):
-    return concatenation_in_set(n1, n2, s) and concatenation_in_set(n2, n1, s)
+    return concatenation_is_prime(n1, n2, s) and concatenation_is_prime(n2, n1, s)
 
 
-def check_primes(primes_to_use, prime_set_to_use):
-    length = len(primes_to_use)
-    for i1 in xrange(length):
-        prime1 = primes_to_use[i1]
-        for i2 in xrange(i1 + 1, length):
-            prime2 = primes_to_use[i2]
-            if not both_permutations_in_set(prime1, prime2, prime_set_to_use):
-                continue
-            for i3 in xrange(i2 + 1, length):
-                prime3 = primes_to_use[i3]
-                if not both_permutations_in_set(prime1, prime3, prime_set_to_use):
-                    continue
-                if not both_permutations_in_set(prime2, prime3, prime_set_to_use):
-                    continue
-                for i4 in xrange(i3 + 1, length):
-                    prime4 = primes_to_use[i4]
-                    if not both_permutations_in_set(prime1, prime4, prime_set_to_use):
-                        continue
-                    if not both_permutations_in_set(prime2, prime4, prime_set_to_use):
-                        continue
-                    if not both_permutations_in_set(prime3, prime4, prime_set_to_use):
-                        continue
-                    for i5 in xrange(i4 + 1, length):
-                        prime5 = primes_to_use[i5]
-                        if not both_permutations_in_set(prime1, prime5, prime_set_to_use):
-                            continue
-                        if not both_permutations_in_set(prime2, prime5, prime_set_to_use):
-                            continue
-                        if not both_permutations_in_set(prime3, prime5, prime_set_to_use):
-                            continue
-                        if not both_permutations_in_set(prime4, prime5, prime_set_to_use):
-                            continue
-                        print 'Found this prime set :: {}, {}, {}, {}'.format(prime1, prime2, prime3, prime4)
-                        return True
-    return False
+def form_adjacency_list(primes, primes_mod_one, primes_mod_two):
+    adjacency_list = {}
+    for source_prime in primes:
+        adjacency_list[source_prime] = set()
+
+        prime_set = primes_mod_one if source_prime % 3 == 1 else primes_mod_two
+        prime_set = filter(lambda x: x > source_prime, prime_set)
+
+        print source_prime, len(prime_set)
+        for destination_prime in prime_set:
+            if both_permutations_in_set(source_prime, destination_prime, primes):
+                adjacency_list[source_prime].add(destination_prime)
+
+    return adjacency_list
 
 
 def solution():
-    primes = sieve_of_eratosthenes(HIGHEST_PRIME)
-    prime_set = set(primes)
+    prime_list = sieve_of_eratosthenes(PRIME_UPPER_BOUND)
+    del [prime_list[0]]
+    primes_mod_one_list = [3] + filter(lambda x: x % 3 == 1, prime_list)
+    primes_mod_two_list = [3] + filter(lambda x: x % 3 == 2 and x != 2, prime_list)
 
-    primes_mod_one = [3] + [i for i in primes if i % 3 == 1]
-    primes_mod_two = [3] + [i for i in primes if i % 3 == 2]
+    primes = set(prime_list)
+    primes_mod_one = set(primes_mod_one_list)
+    primes_mod_two = set(primes_mod_two_list)
 
-    if check_primes(primes_mod_one, prime_set):
-        return
-    if check_primes(primes_mod_two, prime_set):
-        return
+    adjacency_list = form_adjacency_list(primes, primes_mod_one, primes_mod_two)
 
     print 'Could not find prime family -- generate larger primes :('
 
