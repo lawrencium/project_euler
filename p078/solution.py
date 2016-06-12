4e3a7020ca97409774e9ff0e3658220b1758aba1
lawrencium <ltc35@cornell.edu>
2016-06-12 10:56:30 +0200

    started p078

--- p078/solution.py
+++ p078/solution.py
@@ -1,39 +1,53 @@
 from util.solutiontimer import time_function
 
-PERMUTATION_DICTIONARY = {}
+PERMUTATION_TABLE = {}
 
 
-def recursive_calculator(n):
-    # if n <= 1:
-    #     return n
-    #
-    # piles = 0
-    # piles += n / 2 + 1
-    # for i in range(n / 2 + 1):
-    #     remainder = n - i
-    #     piles += recursive_calculator(i)
-    #     piles += recursive_calculator(remainder)
+def internal_recursive_calculator(n):
+    if n <= 1:
+        return 0
 
+    piles = 0
+    for starting_pile in range(1, n / 2 + 1):
+        piles += 1
+        remainder = n - starting_pile
+        # print 'internal', starting_pile, remainder
+        piles += internal_recursive_calculator(remainder)
+    return piles
+
+
+def get_internal_permutations(remainder):
+    if remainder in PERMUTATION_TABLE:
+        return PERMUTATION_TABLE[remainder]
+
+    permutations = internal_recursive_calculator(remainder)
+
+    PERMUTATION_TABLE[remainder] = permutations
+    print 'internal permutations for', remainder, 'was', permutations
+    return permutations
+
+
+def number_piles(n):
     if n == 1:
         return 1
     elif n == 0:
         return 0
 
+    piles = 1
+    print n, 0
+    for starting_pile in range(1, n / 2 + 1):
+        piles += 1
 
-def calculate_number_piles(n):
-    if n == 1:
-        return 1
+        remainder = n - starting_pile
+        print starting_pile, remainder
+        internal_permutations = get_internal_permutations(remainder)
+        piles += internal_permutations
 
-
-def number_piles(n):
-    if n in PERMUTATION_DICTIONARY:
-        return PERMUTATION_DICTIONARY[n]
-    else:
-        calculate_number_piles(n)
+    return piles
 
 
 def main():
-    pass
+    print number_piles(5)
 
 
 if __name__ == '__main__':
