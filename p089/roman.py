eac57c71f11a7bea13b39f5821a584a0567f27f0
lawrencium <ltc35@cornell.edu>
2016-07-21 15:50:43 -0700

    code cleanup by deleting some commented-out code.

--- p089/roman.py
+++ p089/roman.py
@@ -32,8 +32,8 @@ def compress(roman_numeral):
     if all([numeral == 'M' for numeral in roman_numeral]):
         return roman_numeral
 
-    consecutive_occurrences_index = starting_index_of_n_consecutive_occurrences(roman_numeral,
-                                                                                MINIMUM_CONSECUTIVE_OCCURRENCE_FOR_COMPRESSION)
+    consecutive_occurrences_index = starting_index_of_n_consecutive_occurrences(
+        roman_numeral, MINIMUM_CONSECUTIVE_OCCURRENCE_FOR_COMPRESSION)
 
     if consecutive_occurrences_index == -1:
         return roman_numeral
@@ -58,8 +58,3 @@ def replace_consecutive_occurrences(index, roman_numeral):
     else:
         substring_replacement = repeating_character + hierarchical_replacement
         return handle_replacement(index, MINIMUM_CONSECUTIVE_OCCURRENCE_FOR_COMPRESSION, substring_replacement)
-
-        # substring_replacement = repeating_character + CLASSIC_ROMAN_NUMERAL_HIERARCHY[
-        #     hierarchical_replacement] if hierarchical_replacement in roman_numeral else repeating_character + hierarchical_replacement
-        # return roman_numeral.replace(repeating_character * MINIMUM_CONSECUTIVE_OCCURRENCE_FOR_COMPRESSION,
-        #                              substring_replacement, 1)
